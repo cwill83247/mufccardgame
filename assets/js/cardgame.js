6f16ffcaaace72e8772be91eefd4893a6bd27270
cwill83247 <breakthrumusic@hotmail.com>
2022-08-21 14:28:57 +0000

    Add: Added basic JS tavaScript and CSS get card blanks to displaylay

--- assets/js/cardgame.js
+++ assets/js/cardgame.js
@@ -1,29 +1,67 @@
-console.log ("script linked");
-const playerCards = [                  /**Array of all the Cards only 6 needed as will be having 12 in total too match */
+const playerCards = [                  /**Array of all the Cards only 6 unique cards needed as will be having 12 in total too match */
     {
         playerName:"Cantona",
-        playerImage: /assets/images/cancelAnimationFrame.png,
+        playerImage: "/assets/images/cantona-card.fw.png",
     },
     {
-        playerName:"Ronney",
-        playerImage: /assets/images/cancelAnimationFrame.png,
+        playerName:"Rooney",
+        playerImage: "/assets/images/rooney-card-fw.png",
     },
     {
-        playerName:"Cantona",
-        playerImage: /assets/images/cancelAnimationFrame.png,
-    },
-    {
-        playerName:"Cantona",
-        playerImage: /assets/images/cancelAnimationFrame.png,
+        playerName:"Scholes",
+        playerImage: "/assets/images/scholes-card.fw.png",
     },
     {
         playerName:"Cantona",
-        playerImage: /assets/images/cancelAnimationFrame.png,
+        playerImage: "/assets/images/test-card.png",
     },
     {
         playerName:"Cantona",
-        playerImage: /assets/images/cancelAnimationFrame.png,
+        playerImage: "/assets/images/test-card.png",
     },
+    {
+        playerName:"Cantona",
+        playerImage: "/assets/images/test-card.png",
+    },
+    {
+        playerName:"Cantona",
+        playerImage: "/assets/images/cantona-card.fw.png",
+    },
+    {
+        playerName:"Rooney",
+        playerImage: "/assets/images/rooney-card-fw.png",
+    },
+    {
+        playerName:"Scholes",
+        playerImage: "/assets/images/scholes-card.fw.png",
+    },
+    {
+        playerName:"Cantona",
+        playerImage: "/assets/images/test-card.png",
+    },
+    {
+        playerName:"Cantona",
+        playerImage: "/assets/images/test-card.png",
+    },
+    {
+        playerName:"Cantona",
+        playerImage: "/assets/images/test-card.png",
+    }
 
+] /**end of playerCards Array  */
+playerCards.sort (()=> 0.5 - Math.random())        /**this is randomising the order in which the cards display by putting them at different index points in the array **/
 
-]
+const cardGameArea = document.querySelector("#card-game-area")
+
+function createGameArea() {
+    for (let i =0; i <12; i++) {     /*Start of for loop    **/
+        const createPlayerCard = document.createElement("img")
+        createPlayerCard.setAttribute("src", "assets/images/front-card-face.fw.png")  
+        createPlayerCard.setAttribute("data-id", i)                             /**adding a data-id attribute so we can call on it later  **/
+        cardGameArea.appendChild(createPlayerCard)
+        
+    }
+
+}
+
+createGameArea();
