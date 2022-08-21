8199b03407c885cbc7b90486fa98b916410910b7
cwill83247 <breakthrumusic@hotmail.com>
2022-08-21 12:35:29 +0000

    Add: Adding Card Images

--- assets/js/cardgame.js
+++ assets/js/cardgame.js
@@ -1,1 +1,29 @@
 console.log ("script linked");
+const playerCards = [                  /**Array of all the Cards only 6 needed as will be having 12 in total too match */
+    {
+        playerName:"Cantona",
+        playerImage: /assets/images/cancelAnimationFrame.png,
+    },
+    {
+        playerName:"Ronney",
+        playerImage: /assets/images/cancelAnimationFrame.png,
+    },
+    {
+        playerName:"Cantona",
+        playerImage: /assets/images/cancelAnimationFrame.png,
+    },
+    {
+        playerName:"Cantona",
+        playerImage: /assets/images/cancelAnimationFrame.png,
+    },
+    {
+        playerName:"Cantona",
+        playerImage: /assets/images/cancelAnimationFrame.png,
+    },
+    {
+        playerName:"Cantona",
+        playerImage: /assets/images/cancelAnimationFrame.png,
+    },
+
+
+]
